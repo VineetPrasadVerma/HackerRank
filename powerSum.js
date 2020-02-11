b2e2cee1e143fb195cd19760f7641f9ee35f6693
Vineet Prasad Verma <vineetpd1996@gmail.com>
2020-02-11 15:33:36 +0530

    Solved Power Sum Via Recusrion

--- powerSum.js
+++ powerSum.js
@@ -1,19 +1,25 @@
-function powerSum(X, N) {
-  let count = 0
-  for (let i = 1; i <= Math.ceil(X / N); i++) {
-    for (let j = i; j <= Math.ceil(X / N); j++) {
-      if (Math.pow(i, N) + Math.pow(j, N) > X) {
-        break
-      }
-
-      if (Math.pow(i, N) + Math.pow(j, N) === X) {
-        console.log(i, j)
-        count += 1
-      }
-    }
+function powerSum(X, N, num) {
+  // let count = 0
+  // for (let i = 1; i <= Math.ceil(X / N); i++) {
+  //   for (let j = i; j <= Math.ceil(X / N); j++) {
+  //     if (Math.pow(i, N) + Math.pow(j, N) > X) {
+  //       break
+  //     }
+  //     if (Math.pow(i, N) + Math.pow(j, N) === X) {
+  //       console.log(i, j)
+  //       count += 1
+  //     }
+  //   }
+  // }
+  // return count
+  const val = X - Math.pow(num, N)
+  if (val === 0) {
+    return 1
   }
-
-  return count
+  if (val < 0) {
+    return 0
+  }
+  return powerSum(val, N, num + 1) + powerSum(X, N, num + 1)
 }
 
-console.log(powerSum(101, 2))
+console.log(powerSum(100, 2, 1))
